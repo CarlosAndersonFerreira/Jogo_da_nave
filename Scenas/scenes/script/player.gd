f00901b9da509027e8f51c0fb857e61b6f13549d
Aluno EDU FOR 2 <alunofor2@edu.sesc-ce.com.br>
2024-02-28 09:49:54 -0300

    Adicionando jogo da nave

---
+++ Scenas/scenes/script/player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+var speed = 200
+
+func _ready():
+	$AnimatedSprite2D.play()
+	
+func _physics_process(delta):
+	velocity.x = 0
+	velocity.y = 0
+	
+	if Input.is_action_pressed("ui_up"):
+		velocity.y = velocity.y - speed
+	if Input.is_action_pressed("ui_down"):
+		velocity.y = velocity.y + speed	
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = velocity.x - speed
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = velocity.x + speed		
+	
+	move_and_slide()		
+	 	
+
+
+
